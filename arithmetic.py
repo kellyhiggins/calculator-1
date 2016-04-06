455152529ca8a4603dd35119f37603866b6623b3
Hackbright Student <info@hackbrightacademy.com>
2016-04-06 12:17:42 -0700

    add division feature, cleaning up code

--- arithmetic.py
+++ arithmetic.py
@@ -1,20 +1,18 @@
 def add(num1, num2):
     """adds num1 and num2, returning the resulting sum"""
-    added_num = num1 + num2
-    return added_num
+    return num1 + num2
 
 def subtract(num1, num2):
     """subtracts num2 from num1, returning the result"""
-    subtracted_num = num1 - num2
-    return subtracted_num
+    return num1 - num2
 
 def multiply(num1, num2):
     """multiplies num1 and num2, returning the result"""
-    multiplied_num = num1 * num2
-    return multiplied_num
+    return num1 * num2
 
 def divide(num1, num2):
-    pass
+    """divides num2 from num1, returning the result"""
+    return num1 / num2
 
 def square(num1):
     pass
